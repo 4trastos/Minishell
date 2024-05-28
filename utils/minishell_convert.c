17703b0e23f8ba0f81bc55bee6b04531914de5a5
4trastos <93524280+4trastos@users.noreply.github.com>
2024-05-28 16:11:55 +0200

    Todo en una struct

---
+++ utils/minishell_convert.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_convert.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 13:13:54 by nicgonza          #+#    #+#             */
+/*   Updated: 2024/05/26 13:31:16 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/minishell.h"
+#include "../incl/list.h"
+
+static int	ft_transform(const char *str, int i)
+{
+	int	result;
+	int	digit;
+
+	result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		digit = str[i] - '0';
+		result = result * 10 + digit;
+		i++;
+	}
+	return (result);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\f' || str[i] == '\v')
+	{
+		i++;
+	}
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	result = ft_transform(str, i);
+	return (sign * result);
+}
+
+static unsigned int	ft_numlen(int n)
+{
+	unsigned int	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		count++;
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int	len;
+	char			*result;
+	unsigned int	num;
+
+	len = ft_numlen(n);
+	result = (char *)malloc((len + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	if (n < 0)
+	{
+		result[0] = '-';
+		num = -n;
+	}
+	else
+		num = n;
+	if (num == 0)
+		result[0] = '0';
+	result[len] = '\0';
+	while (num != 0)
+	{
+		result[len - 1] = '0' + (num % 10);
+		num = num / 10;
+		len--;
+	}
+	return (result);
+}
+
+char	*ft_strchr_hd(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i] + 1);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
