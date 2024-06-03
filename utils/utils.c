9dd161d39587f7489ea5da16746fa71fbc5ade3b
4trastos <93524280+4trastos@users.noreply.github.com>
2024-06-03 13:29:46 +0200

    Normi OK

---
+++ utils/utils.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 13:13:54 by nicgonza          #+#    #+#             */
+/*   Updated: 2024/05/29 13:07:55 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/minishell.h"
+
+
+int	ft_strlist(char **echo)
+{
+	int	i;
+
+	i = 0;
+	while (echo[i] != NULL)
+		i++;
+	return (i);
+}
+
+void	ft_doublefree(char **echo)
+{
+	int	i;
+
+	i = 0;
+	while (echo[i] != NULL)
+	{
+		free(echo[i]);
+		i++;
+	}
+	free (echo);
+}
+
+int	custom_strncmp(char *prompt, char *built, int len, int blt)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	(void)blt;
+	if (!prompt)
+		return (1);
+	while (i <= len && built[i] != '\0')
+	{
+		if (built[i] != prompt[j])
+			return (1);
+		i++;
+		j++;
+	}
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
